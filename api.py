50ed766ce52f74922145ad10e5bef56a56745f8a
joshwilson1212 <71780851+joshwilson1212@users.noreply.github.com>
2020-11-01 14:19:53 -0600

    Add files via upload
    api info

---
+++ api.py
@@ -0,0 +1,15 @@
+import requests
+
+api_key = "b29344da13414323bac320e823e7736a"
+
+response = requests.get("https://api.spoonacular.com/recipes/complexSearch")
+print (response)
+
+
+# the respons line dose not work
+# this is my api key above here are some links you may needs
+# https://github.com/ddsky/spoonacular-api-clients/blob/master/python/docs/DefaultApi.md#search_recipes_by_ingredients
+# https://github.com/ddsky/spoonacular-api-clients/tree/master/python#apikeyscheme
+# if you need any more info just msg me on snap
+
+    
